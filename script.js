f054c47cf02f4250281d5f6dbeffaed1e75670f6
Rafael Farias <rafaelfsantana21@gmail.com>
2024-11-20 13:21:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let indiceAtual = 0;
+const descs = [
+    '',
+    '',
+    '']
+const imagens = [
+    "imgs/arduino/prototipoDiagonal.jpeg",
+    "imgs/arduino/prototipoDeLado.jpeg",
+    "imgs/arduino/prototipoDeCima.jpeg"];
+function mudarImagem() {
+    if (indiceAtual < imagens.length-1){
+        console.log("Mensagem teste")
+        indiceAtual+=1
+    }
+    else {
+        indiceAtual = 0
+    };
+
+    document.getElementById("desc").innerHTML = descs[indiceAtual];
+    document.getElementById("imagem").src = imagens[indiceAtual];
+}
+
+
+
+
+
+
